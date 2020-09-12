805f8eaac4cf3a6018fac8ba9e363508e99d92d7
Claudney Sarti Sesa <claudneysartisessa@gmail.com>
2020-09-12 19:22:23 -0300

    Ajuste do Main para Utilizar PageView

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_getx/main_controller.dart';
+import 'package:flutter_getx/presentation/page1/page1_view.dart';
+import 'package:flutter_getx/presentation/page2/page2_view.dart';
+import 'package:flutter_getx/presentation/page3/page3_view.dart';
+import 'package:flutter_getx/presentation/page4/page4_view.dart';
+import 'package:flutter_getx/presentation/page5/page5_view.dart';
+import 'package:flutter_getx/presentation/page6/page6_view.dart';
 import 'package:get/get.dart';
-import 'package:get/state_manager.dart';
 
 void main() {
   runApp(
@@ -17,138 +21,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final MainController controller = MainController();
-  final TextEditingController textController = TextEditingController();
-
-  void initState() {
-    textController.clear();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Teste GetX",
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          child: Column(
-            children: [
-              Obx(
-                () {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: textController,
-                      obscureText: controller.status,
-                      obscuringCharacter: "*",
-                      decoration: InputDecoration(
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.access_alarm),
-                          onPressed: () {
-                            controller.setStatus();
-                          },
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 120,
-                      child: RaisedButton(
-                        onPressed: () {
-                          controller.setNome(textController.text);
-                        },
-                        child: Text("Set Nome"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 120,
-                      child: RaisedButton(
-                        onPressed: () {
-                          controller.incContador();
-                        },
-                        child: Text("Set Contador"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 120,
-                      child: RaisedButton(
-                        onPressed: () {
-                          // controller.pessoa["nome"] = textController.text;
-                          // controller.objPessoa.nome = textController.text;
-                          // controller.objPessoa.idade = 36;
-                        },
-                        child: Text("Set Pessoa"),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              GetX<MainController>(
-                builder: (_) {
-                  return Column(
-                    children: [
-                      Divider(),
-                      Text(controller.nome ?? ""),
-                    ],
-                  );
-                },
-              ),
-              Obx(
-                () {
-                  return Column(
-                    children: [
-                      Divider(),
-                      Text(controller?.contador?.toString()),
-                    ],
-                  );
-                },
-              ),
-              // GetX<MainController>(
-              //   builder: (_) {
-              //     return Column(
-              //       children: [
-              //         Divider(),
-              //         Text(controller.pessoa.toString() ?? ""),
-              //       ],
-              //     );
-              //   },
-              // ),
-              // GetX<MainController>(
-              //   builder: (_) {
-              //     return Column(
-              //       children: [
-              //         Divider(),
-              //         Text(controller.objPessoa.nome ?? ""),
-              //       ],
-              //     );
-              //   },
-              // )
-            ],
-          ),
-        ),
-      ),
+    return PageView(
+      children: [
+        Page1(),
+        Page2(),
+        Page3(),
+        Page4(),
+        Page5(),
+        Page6(),
+      ],
     );
   }
 }
